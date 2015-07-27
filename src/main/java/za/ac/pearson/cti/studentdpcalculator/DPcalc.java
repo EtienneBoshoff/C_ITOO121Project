ad39fb0e0a85815e096e90a50bf9140a0b7c470a
Paul1993 <paul.treurnicht@gmail.com>
2015-07-27 21:50:19 +0200

    My 4 tests are working and added extra messages on the main class

--- src/main/java/za/ac/pearson/cti/studentdpcalculator/DPcalc.java
+++ src/main/java/za/ac/pearson/cti/studentdpcalculator/DPcalc.java
@@ -1,42 +1,23 @@
 /*
- * This is the starting program for CTI students in Object Orientated 
- * Programming.  To run this program click on the Run button in the toolbar
- * above (The one that looks like a play button).  In each class you will find
- * instructions on what needs to be done to complete this homework assignment
- * 
- * This is homework assignment: 1
- * It is due: Week of 13 July 2015
- * Remember to hand in all you need to do is upload a picture of all of your
- * JUnit tests passing.
+ *Paul Treurnicht 
+ *DV2012-0003
+ *Windows 8.1
+ *2015/07/23
  */
 package za.ac.pearson.cti.studentdpcalculator;
 
-/**
- * Program description:  This class will calculate a students due performance
- * or DP.  
- * @author Etienne Boshoff - Lecturer CTI Education Group
- * 
- * Task: Complete the methods such that the tests pass.  Note you are not
- * allowed to change the tests in any way.
- */
 public class DPcalc {
     
-    /**
-     * Variables are your attributes associated with your class. For example a
-     * car can be different colours.  The colour blue would then be an attribute
-     * of the class car.  This means that its variable for colour may look as 
-     * follows:
-     * 
-     * String colour = "blue"; 
-     * OR
-     * String colour; if we leave it undefined till a later time
-     */
-    //Variable declarations here
-    //Task: Complete the variable declarations
+    //Variable declarations
     Double assignment;
     Double semesterTest;
     Double continuous;
-    
+    String subject;
+    String dvNum;
+    String name;
+    Double weightSem = 0.20;
+    Double weightAss = 0.60;
+    Double weightCont= 0.20;
     //End of variable declarations
     
     /**
@@ -50,44 +31,25 @@ public class DPcalc {
         assignment = assMark;
         semesterTest = semTestMark;
         continuous = contAssMark;
+            
     }
     
-    /**
-     * This method calculates the students DP for the semester with the values
-     * gathered from the constructor.  A method is the behaviour of an object
-     * it can be seen as doing a specific task.  In the comments I will describe
-     * the task and you are required to complete the method such that it passes
-     * the java unit test.
-     * 
-     * Method: DP is calculated by taking assignment*assWeight + semesterTest*semWeight * continuous*contWeight
-     * 
-     * @return The DP of the student as a double floating point value
-     */
-    //Task: complete this method as described in the comments and to pass the unit test
     public Double calculateDP() {
-        throw new UnsupportedOperationException("You still need to complete this method");
+        
+        double DP = (assignment*weightAss)+(semesterTest*weightSem)+(continuous*weightCont);
+        
+        return DP;
+        
     }
-    
-    /**
-     * This method returns a string that is formatted to look like a nice report
-     * when printed with System.out.println
-     * 
-     * A few things to take note of:
-     * \n gives you a new line
-     * \t gives you a tab
-     * All students received marks and then his calculated DP must be displayed 
-     * as follows
-     * 
-     * Dear student you have attained:
-     * Assignment: -studentmark-% 
-     * SemesterTest: -studentmark-% 
-     * Continuous Assessment: -studentmark-% 
-     * Your DP is calculated as: -calculatedDP-%
-     * 
-     * @return A formatted string
-     */
+
     public String prettyPrintDPreport() {
-        throw new UnsupportedOperationException("You still need to complete this method");
+        String formatedString = "Dear student you have attained:\n"
+        +"Assignment: "+assignment+"%\n"
+        +"Semester test: "+semesterTest+"%\n"
+        +"Continous Assessment: "+continuous+"%\n"
+        +"Your DP is calculated as: "+calculateDP()+"%";
+        
+        return formatedString;
     }
     
     /**
@@ -96,25 +58,54 @@ public class DPcalc {
      * within the calculator
      * @return The assignment mark stored within the calculator
      */
+    
+    //Get attribute from object
     public Double getAssignmentMark() {
         return assignment;
     }
     
-    // Task create the other accessors
+    //Get attribute from object
     public Double getSemesterMark() {
-        throw new UnsupportedOperationException("You still need to complete this method");
+        return semesterTest;
     }
-    
+    //Get attribute from object
     public Double getContinuousAssessmentMark() {
-        throw new UnsupportedOperationException("You still need to complete this method");
+        return continuous;
+    }
+   
+    //Sets the new name variable
+    public void setName(String newName) {
+        name = newName;
     }
     
-    /**
-     * This method checks if you have eligibility to write the exams
-     * Remember you need at least a 40% DP to get exam eligibility
-     * @return True if you can write exams. False otherwise
-     */
+    //Sets the new subject variable
+    public void setSubject(String newSubject) { 
+        subject = newSubject;
+    }
+    
+    //Sets the new DVnum variable
+    public void setDvNum(String newDV) {
+        dvNum = newDV;
+    }
+    
+    //Sets the new semester weight
+    public void setWeightSem(Double newSemWeight) {   
+        weightSem = newSemWeight;
+    }
+    
+    //Sets the new assignment weight
+    public void setWeightAss(Double newAssWeight) {
+        weightSem = newAssWeight;
+    }
+    
+    //Sets the new continuous weight
+    public void setWeightCont(Double newContWeight) {  
+        weightSem = newContWeight;
+    }
+    
+    //Checks if student can write exams
     public Boolean canWriteExams() {
-        throw new UnsupportedOperationException("You still need to complete this method");
+        if (calculateDP() >= 40);
+            return true;
     }
 }
