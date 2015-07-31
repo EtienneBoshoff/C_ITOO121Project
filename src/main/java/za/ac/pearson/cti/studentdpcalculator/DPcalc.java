d65051fb0e092bb113aa820a55219d6d4d5dc0a6
Paul1993 <paul.treurnicht@gmail.com>
2015-07-31 20:30:01 +0200

    My 4 test are working and added extra work

--- src/main/java/za/ac/pearson/cti/studentdpcalculator/DPcalc.java
+++ src/main/java/za/ac/pearson/cti/studentdpcalculator/DPcalc.java
@@ -9,15 +9,29 @@ package za.ac.pearson.cti.studentdpcalculator;
 public class DPcalc {
     
     //Variable declarations
-    Double assignment;
-    Double semesterTest;
-    Double continuous;
-    String subject;
-    String dvNum;
-    String name;
-    Double weightSem = 0.20;
-    Double weightAss = 0.60;
-    Double weightCont= 0.20;
+    private final Double assignment;
+    private Double assignment2;
+    private Double assignment3;
+    private final Double semesterTest;
+    private Double semesterTest2;
+    private Double semesterTest3;
+    private final Double continuous;
+    private Double continuous2;
+    private Double continuous3;
+    private String subject;
+    private String subject2;
+    private String subject3;
+    private String dvNum;
+    private String name;
+    private Double weightSem = 0.20;
+    private Double weightAss = 0.60;
+    private Double weightCont= 0.20;
+    private Double weightSem2;
+    private Double weightAss2;
+    private Double weightCont2;
+    private Double weightSem3;
+    private Double weightAss3;
+    private Double weightCont3;
     //End of variable declarations
     
     /**
@@ -30,26 +44,108 @@ public class DPcalc {
     public DPcalc(Double assMark, Double semTestMark, Double contAssMark) {
         assignment = assMark;
         semesterTest = semTestMark;
-        continuous = contAssMark;
-            
+        continuous = contAssMark;         
     }
     
-    public Double calculateDP() {
-        
-        double DP = (assignment*weightAss)+(semesterTest*weightSem)+(continuous*weightCont);
-        
-        return DP;
-        
+    
+    public DPcalc(Double assMark, Double assMark2, Double assMark3, Double semTestMark,Double semTestMark2,Double semTestMark3, Double contAssMark,Double contAssMark2,Double contAssMark3) {
+        assignment = assMark;
+        assignment2 = assMark2;
+        assignment3 = assMark3;
+        semesterTest = semTestMark;
+        semesterTest2 = semTestMark2;
+        semesterTest3 = semTestMark3;
+        continuous = contAssMark;
+        continuous2 = contAssMark2;
+        continuous3 = contAssMark3;
     }
-
+    /*
+     * @param subName The subject name of the student
+     * @param studNum The student number
+     * @param studName The name of the student
+     * @param assMark The assignment mark of the student in percentage
+     * @param semTestMark The semester test mark of the student in percentage
+     * @param contAssMark The continual assessment mark of the student in percentage
+     * @param wSem The weight of the semester test
+     * @param wAss The weight of the assignment 
+     * @param wCont The weight of the continuous assessment
+    */
+    
+    public DPcalc(Double assMark,Double assMark2,Double assMark3, Double semTestMark,Double semTestMark2,Double semTestMark3,
+            Double contAssMark,Double contAssMark2, Double contAssMark3,String subName,String subName2,String subName3, String studNum,String studName, 
+            Double wSem, Double wAss, Double wCont, Double wSem2, Double wAss2, Double wCont2, Double wSem3, Double wAss3, Double wCont3){
+        assignment = assMark;
+        assignment2 = assMark2;
+        assignment3 = assMark3;
+        semesterTest = semTestMark;
+        semesterTest2 = semTestMark2;
+        semesterTest3 = semTestMark3;
+        continuous = contAssMark;
+        continuous2 = contAssMark2;
+        continuous3 = contAssMark3;
+        subject = subName;
+        subject2 = subName2;
+        subject3 = subName3;
+        dvNum = studNum;
+        name = studName;
+        weightSem = wSem;
+        weightAss = wAss;
+        weightCont = wCont;
+        weightSem2 = wSem2;
+        weightAss2 = wAss2;
+        weightCont2 = wCont2;
+        weightSem3 = wSem3;
+        weightAss3 = wAss3;
+        weightCont3 = wCont3;
+    }
+    
+    
+    //Print a report view of all the students information
     public String prettyPrintDPreport() {
         String formatedString = "Dear student you have attained:\n"
-        +"Assignment: "+assignment+"%\n"
-        +"Semester test: "+semesterTest+"%\n"
-        +"Continous Assessment: "+continuous+"%\n"
+        +"Assignment: "+getAssignmentMark()+"%\n"
+        +"Semester test: "+getSemesterMark()+"%\n"
+        +"Continous Assessment: "+getContinuousAssessmentMark()+"%\n"
         +"Your DP is calculated as: "+calculateDP()+"%";
         
         return formatedString;
+    }
+    
+    //Print a report view of all the students information   
+    public String printFull() {
+        String fullString = "Dear "+name+" "+dvNum+" you have attained the following mark for "+subject+":\n"
+        +"\n"
+        +"Assignment: "+getAssignmentMark()+"%\n"
+        +"Assignment weight: "+getAssignmentWeight()+"%\n"
+        +"Semester test: "+getSemesterMark()+"%\n"
+        +"Semester test weight: "+getSemesterTestWeight()+"%\n"
+        +"Continous Assessment: "+getContinuousAssessmentMark()+"%\n"       
+        +"Continous Assessment weight: "+getContinuousWeight()+"%\n"
+        +"\n"
+        +"Your DP for "+subject+" is: "+calculateDP()/100+"%\n"
+        +"\n"
+        +"Your "+subject2+" information\n"
+        +"\n"
+        +"Assignment: "+getAssignmentMark2()+"%\n"
+        +"Assignment weight: "+getAssignmentWeight2()+"%\n"
+        +"Semester test: "+getSemesterMark2()+"%\n"
+        +"Semester test weight: "+getSemesterTestWeight2()+"%\n"
+        +"Continous Assessment: "+getContinuousAssessmentMark2()+"%\n" 
+        +"Continous Assessment weight: "+getContinuousWeight2()+"%\n"
+        +"\n"
+        +"Your DP for "+subject2+" is: "+calculateDP2()/100+"%\n"
+        +"\n"
+        +"Assignment: "+getAssignmentMark3()+"%\n"
+        +"Assignment weight: "+getAssignmentWeight3()+"%\n"
+        +"Semester test: "+getSemesterMark3()+"%\n"
+        +"Semester test weight: "+getSemesterTestWeight3()+"%\n"
+        +"Continous Assessment: "+getContinuousAssessmentMark3()+"%\n" 
+        +"Continous Assessment weight: "+getContinuousWeight3()+"%\n"
+        +"\n"
+        +"Your DP for "+subject3+" is: "+calculateDP3()/100+"%\n"
+
+        ;
+        return fullString;
     }
     
     /**
@@ -58,6 +154,47 @@ public class DPcalc {
      * within the calculator
      * @return The assignment mark stored within the calculator
      */
+    //DP of the first subject
+    public Double calculateDP() {
+        
+        double DP = (assignment*weightAss)+(semesterTest*weightSem)+(continuous*weightCont);
+        
+        return DP;
+        
+    }
+
+    //DP of the second subject
+    public Double calculateDP2() {
+        
+        double DP = (assignment2*weightAss2)+(semesterTest2*weightSem2)+(continuous2*weightCont2);
+        
+        return DP;
+        
+    }
+    
+    //DP of the third subject
+    public Double calculateDP3() {
+        
+        double DP = (assignment3*weightAss3)+(semesterTest3*weightSem3)+(continuous3*weightCont3);
+        
+        return DP;
+        
+    }
+    
+    //Get attribute from object
+    public String getSubject() {
+        return subject;
+    }
+    
+    //Get attribute from object
+    public String getSubject2() {
+        return subject2;
+    }
+    
+    //Get attribute from object
+    public String getSubject3() {
+        return subject3;
+    }
     
     //Get attribute from object
     public Double getAssignmentMark() {
@@ -65,12 +202,88 @@ public class DPcalc {
     }
     
     //Get attribute from object
+    public Double getAssignmentMark2() {
+        return assignment2;
+    }
+    
+    //Get attribute from object
+    public Double getAssignmentMark3() {
+        return assignment3;
+    }
+    
+    //Get attribute from object
     public Double getSemesterMark() {
         return semesterTest;
     }
+    
+    //Get attribute from object
+    public Double getSemesterMark2() {
+        return semesterTest2;
+    }
+    
+    //Get attribute from object
+    public Double getSemesterMark3() {
+        return semesterTest3;
+    }
+    
     //Get attribute from object
     public Double getContinuousAssessmentMark() {
         return continuous;
+    }
+    
+    //Get attribute from object
+    public Double getContinuousAssessmentMark2() {
+        return continuous2;
+    }
+    
+    //Get attribute from object
+    public Double getContinuousAssessmentMark3() {
+        return continuous3;
+    }
+    
+    //Get attribute from object
+    public Double getAssignmentWeight(){
+        return weightAss;
+    }
+    
+    //Get attribute from object
+    public Double getAssignmentWeight2(){
+        return weightAss2;
+    }
+    
+    //Get attribute from object
+    public Double getAssignmentWeight3(){
+        return weightAss3;
+    }
+    
+    //Get attribute from object
+    public Double getSemesterTestWeight(){
+        return weightSem;
+    }
+    
+    //Get attribute from object
+    public Double getSemesterTestWeight2(){
+        return weightSem2;
+    }
+    
+    //Get attribute from object
+    public Double getSemesterTestWeight3(){
+        return weightSem3;
+    }
+    
+    //Get attribute from object
+    public Double getContinuousWeight(){
+        return weightCont;
+    }
+    
+    //Get attribute from object
+    public Double getContinuousWeight2(){
+        return weightCont2;
+    }
+    
+    //Get attribute from object
+    public Double getContinuousWeight3(){
+        return weightCont3;
     }
    
     //Sets the new name variable
@@ -83,29 +296,73 @@ public class DPcalc {
         subject = newSubject;
     }
     
+    //Sets the new subject variable
+    public void setSubject2(String newSubject2) { 
+        subject2 = newSubject2;
+    }
+    
+    //Sets the new subject variable
+    public void setSubject3(String newSubject3) { 
+        subject3 = newSubject3;
+    }
+    
     //Sets the new DVnum variable
     public void setDvNum(String newDV) {
         dvNum = newDV;
     }
     
-    //Sets the new semester weight
+    //Sets the semester weight
     public void setWeightSem(Double newSemWeight) {   
         weightSem = newSemWeight;
     }
     
-    //Sets the new assignment weight
-    public void setWeightAss(Double newAssWeight) {
-        weightSem = newAssWeight;
+    //Sets the semester weight
+    public void setWeightSem2(Double newSemWeight2) {   
+        weightSem2 = newSemWeight2;
     }
     
-    //Sets the new continuous weight
+    //Sets the semester weight
+    public void setWeightSem3(Double newSemWeight3) {   
+        weightSem3 = newSemWeight3;
+    }
+    
+
+    //Sets the assignment weight
+    public void setWeightAss(Double newAssWeight) {
+        weightAss = newAssWeight;
+    }
+    
+    //Sets the second assignment weight
+    public void setWeightAss2(Double newAssWeight2) {
+        weightAss2 = newAssWeight2;
+    }
+    
+    //Sets the third assignment weight
+    public void setWeightAss3(Double newAssWeight3) {
+        weightAss3 = newAssWeight3;
+    }
+    //Sets the continuous weight
     public void setWeightCont(Double newContWeight) {  
-        weightSem = newContWeight;
+        weightCont = newContWeight;
+    }
+    
+    //Sets the second continuous weight
+    public void setWeightCont2(Double newContWeight2) {  
+        weightCont2 = newContWeight2;
+    }
+    
+    //Sets the second continuous weight
+    public void setWeightCont3(Double newContWeight3) {  
+        weightCont3 = newContWeight3;
     }
     
     //Checks if student can write exams
     public Boolean canWriteExams() {
-        if (calculateDP() >= 40);
-            return true;
+        return calculateDP() >= 0.40;
     }
+    
+    //public Boolean verifyDvNum(String dvNum){
+
+    //}
 }
+//dvNum.contains("DV")&& dvNum.contains(number)&& dvNum.contains("-") 
--- src/main/java/za/ac/pearson/cti/studentdpcalculator/Main.java
+++ src/main/java/za/ac/pearson/cti/studentdpcalculator/Main.java
@@ -45,67 +45,180 @@ public class Main {
         System.out.println("");
         
         //Asks user to input assignment weight
-        System.out.print("Enter your assignment weight: ");
+        System.out.print("Enter your "+subject+" assignment weight: ");
         //Re-assigns keyboard input to userInput variable
         userInput = keyboardInput.nextLine();
         //Saves the users input to the weightAss variable
         Double weightAss = Double.parseDouble(userInput);
-        //Makes value liable to multiply
-        weightAss = weightAss / 100;
         //Prints a blank line
         System.out.println("");
         
         //Asks user to input semester test weight
-        System.out.print("Enter your semester test weight: ");
+        System.out.print("Enter your "+subject+" semester test weight: ");
         //Re-assigns keyboard input to userInput variable
         userInput = keyboardInput.nextLine();
         //Saves the users input to the weightSem variable
         Double weightSem = Double.parseDouble(userInput);
-        //Makes value liable to multiply
-        weightSem = weightSem / 100;
         //Prints a blank line
         System.out.println("");
         
         //Asks user to inpput continuous assessment weight
-        System.out.print("Enter your continuous assessment weight: ");
+        System.out.print("Enter your "+subject+" continuous assessment weight: ");
         //Re-assigns keyboard input to userInput variable
         userInput = keyboardInput.nextLine();
         //Saves the users input to the weightCont variable
         Double weightCont = Double.parseDouble(userInput);
-        //Makes value liable to multiply
-        weightCont = weightCont / 100;
         //Prints a blank line
         System.out.println("");
         
         //Asks user to input assignment mark
-        System.out.print("Please enter the percentage you received for your Assignment: ");
+        System.out.print("Please enter the percentage you received for "+subject+" Assignment: ");
         //Re-assigns keyboard input to userInput variable
         userInput = keyboardInput.nextLine();
         //Saves the users input to the assignmentMark variable
-        Double assignmentMark = Double.parseDouble(userInput);
+        Double assMark = Double.parseDouble(userInput);
         //Prints a blank line
         System.out.println("");
         
         //Asks the user to input semester test mark
-        System.out.print("Please enter the percentage you received for your Semester Test: ");
+        System.out.print("Please enter the percentage you received for "+subject+" Semester Test: ");
         //Re-assigns keyboard input to userInput variable
         userInput = keyboardInput.nextLine();
         //Saves the users input to the semesterTestMark variable
-        Double semesterTestMark = Double.parseDouble(userInput);
+        Double semTestMark = Double.parseDouble(userInput);
         //Prints a blank line
         System.out.println("");
         
         //Asks the user to enter continuous assessment mark
-        System.out.print("Please enter the percentage you received for continuous assessment: ");
+        System.out.print("Please enter the percentage you received for "+subject+" continuous assessment: ");
         //Re-assigns keyboard input to userInput variable
         userInput = keyboardInput.nextLine();
         //Saves the users input to the continuousAssMark variable
-        Double continuousAssMark = Double.parseDouble(userInput);
+        Double contAssMark = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        System.out.print("Enter your second subject name: ");
+        //Assign keyboard input to variable subject
+        String subject2 = keyboardInput.nextLine();
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks user to input assignment weight
+        System.out.print("Enter your "+subject2+" assignment weight: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the weightAss variable
+        Double weightAss2 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks user to input semester test weight
+        System.out.print("Enter your "+subject2+" semester test weight: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the weightSem variable
+        Double weightSem2 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks user to inpput continuous assessment weight
+        System.out.print("Enter your "+subject2+" continuous assessment weight: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the weightCont variable
+        Double weightCont2 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks user to input assignment mark
+        System.out.print("Please enter the percentage you received for "+subject2+" Assignment: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the assignmentMark variable
+        Double assMark2 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks the user to input semester test mark
+        System.out.print("Please enter the percentage you received for "+subject2+" Semester Test: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the semesterTestMark variable
+        Double semTestMark2 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks the user to enter continuous assessment mark
+        System.out.print("Please enter the percentage you received for "+subject2+" continuous assessment: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the continuousAssMark variable
+        Double contAssMark2 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        System.out.print("Enter your third subject name: ");
+        //Assign keyboard input to variable subject
+        String subject3 = keyboardInput.nextLine();
+        //Prints a blank line
+        System.out.println("");
+
+        //Asks user to input assignment weight
+        System.out.print("Enter your "+subject3+" assignment weight: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the weightAss variable
+        Double weightAss3 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks user to input semester test weight
+        System.out.print("Enter your "+subject3+" semester test weight: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the weightSem variable
+        Double weightSem3 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks user to inpput continuous assessment weight
+        System.out.print("Enter your "+subject3+" continuous assessment weight: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the weightCont variable
+        Double weightCont3 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        System.out.print("Please enter the percentage you received for "+subject3+" Assignment : ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the assignmentMark variable
+        Double assMark3 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+                //Asks the user to input semester test mark
+        System.out.print("Please enter the percentage you received for "+subject3+" Semester Test: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the semesterTestMark variable
+        Double semTestMark3 = Double.parseDouble(userInput);
+        //Prints a blank line
+        System.out.println("");
+        
+        //Asks the user to enter continuous assessment mark
+        System.out.print("Please enter the percentage you received for "+subject3+" continuous assessment: ");
+        //Re-assigns keyboard input to userInput variable
+        userInput = keyboardInput.nextLine();
+        //Saves the users input to the continuousAssMark variable
+        Double contAssMark3 = Double.parseDouble(userInput);
         //Prints a blank line
         System.out.println("");
         
         //Calls the DPcalc constructor from the DPcalc Class
-        DPcalc dPcalc = new DPcalc(assignmentMark, semesterTestMark, continuousAssMark);
+        DPcalc dPcalc = new DPcalc(assMark, assMark2, assMark3, semTestMark, semTestMark2, semTestMark3, contAssMark, contAssMark2, contAssMark3);
         
         //Sets keyboardInput value to variable
         dPcalc.setName(name);
@@ -114,11 +227,18 @@ public class Main {
         dPcalc.setWeightAss(weightAss);
         dPcalc.setWeightSem(weightSem);
         dPcalc.setWeightCont(weightCont);
-        
+        dPcalc.setSubject2(subject2);
+        dPcalc.setWeightAss2(weightAss2);
+        dPcalc.setWeightSem2(weightSem2);
+        dPcalc.setWeightCont2(weightCont2);
+        dPcalc.setSubject3(subject3);
+        dPcalc.setWeightAss3(weightAss3);
+        dPcalc.setWeightSem3(weightSem3);
+        dPcalc.setWeightCont3(weightCont3);
         //Prints blank line
         System.out.println("");
         //Prints prettyPrintDPreport
-        System.out.print(dPcalc.prettyPrintDPreport());
+        System.out.print(dPcalc.printFull());
     }
     
 }
