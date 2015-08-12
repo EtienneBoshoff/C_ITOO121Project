310b596b702785cdde55ef3f64e3449b94147d68
ArnoBurger <burger.arno01@gmail.com>
2015-08-12 19:01:04 +0200

    Revert "25 out of 26 tests are working struggeling with the last one."
    This reverts commit 893a4e495d65e2862e7b8e32341c5e1ca6070327.

--- src/main/java/za/ac/pearson/cti/studentdpcalculator/DPcalc.java
+++ src/main/java/za/ac/pearson/cti/studentdpcalculator/DPcalc.java
@@ -11,11 +11,6 @@
  */
 package za.ac.pearson.cti.studentdpcalculator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Program description:  This class will calculate a students due performance
  * or DP.  
@@ -25,7 +20,6 @@ import java.util.regex.Pattern;
  * allowed to change the tests in any way.
  */
 public class DPcalc {
-
     
     /**
      * Variables are your attributes associated with your class. For example a
@@ -39,12 +33,11 @@ public class DPcalc {
      */
     //Variable declarations here
     //Task: Complete the variable declarations
+    Double assignment;
+    Double semesterTest;
+    Double continuous;
     
-    private String studentName;
-    private String studentDVnumber;
-    private List<Subject> subject = new ArrayList<>(); 
     //End of variable declarations
-   
     
     /**
      * This is the constructor for DPcalc.  A constructor is used to create an
@@ -54,30 +47,11 @@ public class DPcalc {
      * @param contAssMark The continual assessment mark of the student in percentage
      */
     public DPcalc(Double assMark, Double semTestMark, Double contAssMark) {
-       this.subject.add(new Subject("DP", semTestMark, contAssMark, assMark, 0.2, 0.2, 0.6));
-        studentName = "Default Student";
-        studentDVnumber = "N/A";
+        assignment = assMark;
+        semesterTest = semTestMark;
+        continuous = contAssMark;
     }
-
-    /**
-     * This is the secondary constructor for DPcalc
-     * @param assignment The assignment mark of the student in percentage
-     * @param semesterTest The semester test mark of the student in percentage
-     * @param continuous The continual assessment mark of the student in percentage
-     * @param studentName The name of the student for whom we are calculating the DP
-     * @param subject The subject the DP is calculated for
-     * @param studentDVnumber The student's DV number
-     * @param assignmentWeight The weight as a floating point percentage
-     * @param semesterTestWeight The weight as a floating point percentage
-     * @param continousAssessmentWeight The weight as a floating point percentage
-     */
-    public DPcalc(Double assignment, Double semesterTest, Double continuous, String studentName, String studentDVnumber, String subject, Double assignmentWeight, Double semesterTestWeight, Double continousAssessmentWeight) {
-        this.subject.add(new Subject(subject, semesterTest, continuous, assignment, semesterTestWeight, continousAssessmentWeight, assignmentWeight));
-        this.studentName = studentName;
-        this.studentDVnumber = studentDVnumber;
-    }
-          //String name, Double semesterTestMark, Double continuousAssessmentMark, Double assessmentMark, Double semesterTestWeight, Double continuousAssessmentWeight, Double assessmentWeight
-       
+    
     /**
      * This method calculates the students DP for the semester with the values
      * gathered from the constructor.  A method is the behaviour of an object
@@ -91,9 +65,7 @@ public class DPcalc {
      */
     //Task: complete this method as described in the comments and to pass the unit test
     public Double calculateDP() {
-        
-         Double DP = getAssignmentMark()*getAssignmentWeight() + getSemesterMark()*getSemesterTestWeight() + getContinuousAssessmentMark()*getContinuousAssessmentWeight();
-        return DP;
+        throw new UnsupportedOperationException("You still need to complete this method");
     }
     
     /**
@@ -115,18 +87,9 @@ public class DPcalc {
      * @return A formatted string
      */
     public String prettyPrintDPreport() {
-        String report = "Dear student you have attained:\n"
-                + "Assignment: "+getAssignmentMark()+"%\n"
-                + "Semester test: "+getSemesterMark()+"%\n"
-                + "Continous Assessment: "+getContinuousAssessmentMark()+"%\n"
-                + "Your DP is calculated as: "+calculateDP()+"%";
-        return report;
+        throw new UnsupportedOperationException("You still need to complete this method");
     }
     
-    public String prettyFullPrint(){
-        System.out.println("Have a great day");
-        return null;
-    }
     /**
      * This method is known as an accessor method or getter.  It allows you to 
      * get an attribute from an object.  In this case the Assignment mark stored
@@ -134,112 +97,24 @@ public class DPcalc {
      * @return The assignment mark stored within the calculator
      */
     public Double getAssignmentMark() {
-        return subject.get(0).getAssignmentMark();
+        return assignment;
     }
     
     // Task create the other accessors
     public Double getSemesterMark() {
-        return subject.get(0).getSemesterTest();
-    }
-    
-    public Double getAssignmentWeight(){
-        return this.subject.get(0).getWeightAss();
-    }
-    
-    public Double getSemesterTestWeight(){
-        return this.subject.get(0).getWeightTest();
-    }
-    
-    public Double getContinuousAssessmentWeight(){
-        return this.subject.get(0).getWeightCont();
-    }
-    
-    public Double getSemesterTestMark(String subject) {
-        return this.subject.get(collectSubject(subject)).getSemesterTest();
-    }
-    
-    public Double getAssignmentMark(String subject) {
-        return this.subject.get(collectSubject(subject)).getAssignmentMark();
-    }
-    
-    public Double getContinuousAssessmentMark(String subject) {
-        return this.subject.get(collectSubject(subject)).getContinuousMark();
+        throw new UnsupportedOperationException("You still need to complete this method");
     }
     
     public Double getContinuousAssessmentMark() {
-       return subject.get(0).getContinuousMark();
+        throw new UnsupportedOperationException("You still need to complete this method");
     }
     
-    public void setWeightAss(Double weightAss) {
-       Subject subs = subject.get(0);
-       subs.setWeightAss(weightAss);
-       subject.set(0, subs);
-    }
-    
-    public void setWeightTest(Double weightTest) {
-        Subject subs = subject.get(0);
-        subs.setWeightTest(weightTest);
-        subject.set(0,subs);
-    }
-    
-    public void setWeightCont(Double weightCont) {
-        Subject subs = subject.get(0);
-        subs.setWeightCont(weightCont);
-        subject.set(0,subs);
-    }
-    
-    private Integer collectSubject(String sub){
-        for (int i = 0; i < subject.size(); i++) {
-            if (subject.get(i).getName().equalsIgnoreCase(sub))
-            {
-                return i;
-            }
-        }    
-        return -10;
-}
     /**
      * This method checks if you have eligibility to write the exams
      * Remember you need at least a 40% DP to get exam eligibility
      * @return True if you can write exams. False otherwise
      */
     public Boolean canWriteExams() {
-        return calculateDP()>= 40.0;
-    }//this method checks whether the student's DP is high enough to write exams displays true or false
-    
-    /**
-     * This method checks if you have a valid DV student number
-     * A student number must be in the following pattern
-     * "DV[yearEnrolled]-[fourDigits]"
-     * @return 
-     */
-    public Boolean verifyDVnum() {
-        if (studentDVnumber.length() == 11){
-            String d = studentDVnumber.charAt(0)+"";
-            String v = studentDVnumber.charAt(1)+"";
-            String year = studentDVnumber.substring(2, 6);
-            String dash = studentDVnumber.charAt(6)+"";
-            String lastNum = studentDVnumber.substring(7);
-            
-            if(d.equalsIgnoreCase("d") && 
-                   v.equalsIgnoreCase("v") && 
-                   year.matches("^\\d+$") && 
-                   dash.equals("-") && 
-                   lastNum.matches("^\\d+$"))
-            {
-                return true;
-            }
-            else 
-            {
-                return false;
-            }
-        } 
-        else 
-        {
-        return false;
-        }
-    }
-
-    public void addSubject(Subject subject) {
-        this.subject.add(subject);
+        throw new UnsupportedOperationException("You still need to complete this method");
     }
 }
