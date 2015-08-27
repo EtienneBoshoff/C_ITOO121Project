b0482ba331808e825bbd387c7df6ec2e192fd3ec
Phil-Venter <wianpventer@gmail.com>
2015-08-27 20:32:29 +0200

    Final commit for today [27/08/15], few bug fixes

--- src/main/java/za/ac/pearson/cti/studentdpcalculator/DPcalcGUIframe.java
+++ src/main/java/za/ac/pearson/cti/studentdpcalculator/DPcalcGUIframe.java
@@ -14,7 +14,6 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -32,7 +31,6 @@ public class DPcalcGUIframe extends JFrame {
             setSize(480,560);
             setLocationRelativeTo(null);
             //------------------------------------------------------------------
-        
         JButton calcBtn = new JButton("Calculate");
         JButton multiSubjectBtn = new JButton("Multiple Subjects");
         JButton resetBtn = new JButton("Reset");
@@ -82,7 +80,7 @@ public class DPcalcGUIframe extends JFrame {
         nameTxtField.setBounds(10, 120, 200, 20);       dvNumTxtField.setBounds(250, 120, 200, 20);
         semMarkLbl.setBounds(10, 160, 200, 20);         semMarkTxtField.setBounds(250, 160, 200, 20);
         conMarkLbl.setBounds(10, 180, 200, 20);         conMarkTxtField.setBounds(250, 180, 200, 20);
-        assMarkLbl.setBounds(10, 20, 200, 20);         assMarkTxtField.setBounds(250, 200, 200, 20);
+        assMarkLbl.setBounds(10, 20, 200, 20);          assMarkTxtField.setBounds(250, 200, 200, 20);
         semWeightLbl.setBounds(10, 240, 200, 20);       semWeightTxtField.setBounds(250, 240, 200, 20);
         conWeightLbl.setBounds(10, 260, 200, 20);       conWeightTxtField.setBounds(250, 260, 200, 20);
         assWeightLbl.setBounds(10, 280, 200, 20);       assWeightTxtField.setBounds(250, 280, 200, 20);
@@ -126,22 +124,40 @@ public class DPcalcGUIframe extends JFrame {
         //calculate button
         calcBtn.addActionListener((ActionEvent evt) -> {
             try {
+                
                 Double assignmentMark = Double.parseDouble(assMarkTxtField.getText());
                 Double semesterTestMark = Double.parseDouble(semMarkTxtField.getText());
                 Double continuousAssessmentMark = Double.parseDouble(conMarkTxtField.getText());
                 Double assignmentWeight = Double.parseDouble(assWeightTxtField.getText());
                 Double semesterTestWeight = Double.parseDouble(semWeightTxtField.getText());
                 Double continuousAssessmentWeight = Double.parseDouble(conWeightTxtField.getText());
-                if (assignmentMark <= 100.0 && semesterTestMark <= 100.0 && continuousAssessmentMark <= 100.0){
-                    if ((assignmentWeight + semesterTestWeight + continuousAssessmentWeight) <= 1.0){
+                
+                if (assignmentMark <= 100.0 && 
+                        semesterTestMark <= 100.0 && 
+                        continuousAssessmentMark <= 100.0 &&
+                        assignmentWeight <= 1.0 && 
+                        semesterTestWeight <= 1.0 && 
+                        continuousAssessmentWeight <= 1.0 && 
+                        assignmentWeight <= 0.0 && 
+                        semesterTestWeight <= 0.0 && 
+                        continuousAssessmentWeight <= 0.0){
+                    
+                    if ((assignmentWeight + semesterTestWeight + continuousAssessmentWeight) <= 1.0 && (assignmentWeight + semesterTestWeight + continuousAssessmentWeight) >= 0.0){
+                        
                         DPcalc calc = new DPcalc(assignmentMark, semesterTestMark, continuousAssessmentMark, "Student", "DV2015-0000", subjectCmboBox.getSelectedItem().toString(), assignmentWeight, semesterTestWeight, continuousAssessmentWeight);
+                        
                         if (calc.canWriteExams()){
+                            
                             passFailLbl.setText("Pass");
                             emoticon.setIcon( new ImageIcon(ImageIO.read(this.getClass().getClassLoader().getResource("ay.jpg"))));
+                            
                         } else {
+                            
                             passFailLbl.setText("Fail");
                             emoticon.setIcon( new ImageIcon(ImageIO.read(this.getClass().getClassLoader().getResource("no.jpg"))));
+                            
                         }
+                        
                         percLbl.setText("Percentage Achieved: " + calc.calculateDP() + "%");
                     }
                 }
@@ -156,6 +172,7 @@ public class DPcalcGUIframe extends JFrame {
         //reset Button
         resetBtn.addActionListener((ActionEvent evt) -> {
             try {
+                
                 semMarkTxtField.setText("");
                 conMarkTxtField.setText("");
                 assMarkTxtField.setText("");
@@ -165,6 +182,7 @@ public class DPcalcGUIframe extends JFrame {
                 passFailLbl.setText("Pass/Fail");
                 percLbl.setText("Percentage Achieved: ");
                 emoticon.setIcon( new ImageIcon(ImageIO.read(this.getClass().getClassLoader().getResource("eh.jpg"))));
+                
             } catch (IOException ex) {
                 System.out.println(ex);
             }
